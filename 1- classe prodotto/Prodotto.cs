c0883e7b82900a8704c25d10847de662d9a5abe0
damora.20465 <damora.20465@E2-9-PC18>
2023-09-19 13:57:02 +0200

    Aggiungere i file di progetto.

---
+++ 1- classe prodotto/Prodotto.cs
@@ -0,0 +1,70 @@
+﻿//using System;
+
+namespace _19_09_23
+{
+	class Prodotto
+	{
+		private string nome;
+		private float prezzo;
+
+		public Prodotto()
+		{
+			nome = "nessun prodotto";
+			prezzo = 0.0f;
+		}
+
+		public Prodotto(string nome, float prezzo)
+		{
+			/*
+			this.nome = nome;
+			this.prezzo = prezzo;
+			*/
+			if(!SetProdotto(nome, prezzo))
+			{
+				this.nome = "inserimento non valido, [throw]";
+				this.prezzo = 0.0f;
+			}
+		}
+
+		public bool SetNome(string nome)
+		{
+			if(string.IsNullOrEmpty(nome)) return false;
+			this.nome = nome;
+			return true;
+		}
+
+		public bool SetPrezzo(float prezzo)
+		{
+			if(prezzo <= 0.0f) return false;
+			this.prezzo = prezzo;
+			return true;
+		}
+
+		public bool SetPrezzo(string prezzo_stringa)
+		{
+			if(float.TryParse(prezzo_stringa, out float prezzo) || prezzo <= 0.0f)
+			{
+				this.prezzo = prezzo;
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public bool SetProdotto(string nome, float prezzo)
+		{
+			return SetNome(nome) & SetPrezzo(prezzo);
+		}
+
+		public string GetNome()
+		{
+			return nome;
+		}
+
+		public float GetPrezzo()
+		{
+			return prezzo;
+		}
+
+	}
+}
---
+++ 1- classe prodotto/Starter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _19_09_23
+{
+	internal class Starter
+	{
+		/*
+		 * Creare la classe prodotto contenente:
+		 * - gli attributi nome e prezzo
+		 * - costruttore senza parametri e con parametri
+		 * - metodi che restituiscano il valore degli attributi
+		 * 
+		 * Creare il main che istanzia due oggetti di tipo prodotto e ne stampi il valore degli attributi
+		*/
+
+		static void Main()
+		{
+			Write("Nome prodotto: ");
+			string name = ReadLine();
+			Write("Prezzo prodotto: ");
+			float price = float.Parse(ReadLine());
+
+			/*
+			while(!float.TryParse($"{ReadLine()}", out price) || price < 0.0f)
+			{//bad input
+				Task.Delay(300).Wait();
+				SetCursorPosition(2, 2);
+				Write($"numero intero positivo");
+				SetCursorPosition(0, 1);
+				Write(" ");
+				CursorLeft--;
+			}
+			Task.Delay(500).Wait();
+			*/
+
+			Prodotto pro1 = new Prodotto(name, price);
+
+			Write("Nome prodotto: ");
+			name = ReadLine();
+			Write("Prezzo prodotto: ");
+			price = float.Parse(ReadLine());
+
+			Prodotto pro2 = new Prodotto(name, price);
+			Prodotto pro0 = new Prodotto();
+
+			Write(Environment.NewLine);
+			WriteLine("Nome prodotto1: " + pro1.GetNome() + "\nPrezzo: " + pro1.GetPrezzo());
+			WriteLine("Nome prodotto2: " + pro2.GetNome() + "\nPrezzo: " + pro2.GetPrezzo());
+			WriteLine("Nome prodotto2: " + pro0.GetNome() + "\nPrezzo: " + pro0.GetPrezzo());
+
+			if(pro0.SetProdotto("testo di esempio", 99.99f))
+				WriteLine("Nome prodotto2: " + pro0.GetNome() + "\nPrezzo: " + pro0.GetPrezzo());
+			else
+				WriteLine("error");
+		}
+	}
+}
